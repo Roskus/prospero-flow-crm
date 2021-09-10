07406c632be8543324fe4e55110b0f73dbe04eeb
Gustavo Novaro <gnovaro@gmail.com>
2021-09-10 20:59:13 +0200

    Add status column for accounts

---
+++ database/migrations/2021_09_10_185216_alter_table_account_add_status_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableAccountAddStatusColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('account', function (Blueprint $table) {
+            $table->unsignedTinyInteger('status')->default('1')->after('amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('account', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
