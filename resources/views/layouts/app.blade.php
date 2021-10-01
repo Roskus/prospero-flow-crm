90bab891649b046ba0240ea7c8cf8117840db418
Gustavo Novaro <gnovaro@gmail.com>
2021-10-01 18:30:12 +0200

    Set application name configurable in the .env file APP_NAME

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <header>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
           <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Hammer') }}</a>
+            <a class="navbar-brand" href="{{ url('/') }}">{{ env('APP_NAME') }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
