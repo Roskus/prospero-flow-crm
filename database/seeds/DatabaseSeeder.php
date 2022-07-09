fff20944cf362245a401a8b2cc45ff076d5e6113
Gustavo Novaro <gnovaro@users.noreply.github.com>
2022-07-09 20:44:24 +0200

    Try to fix db seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,11 +1,15 @@
 <?php
-declare(strict_types = 1);
+//declare(strict_types = 1);
 
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Run the database seeds.
      *
