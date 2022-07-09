fa200a6745a0ffa812a78513c864ee3be3e03da2
Gustavo Novaro <gnovaro@users.noreply.github.com>
2022-07-09 21:14:46 +0200

    Add Lead model

---
+++ app/Models/Lead.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Lead extends Model
+{
+    const ACTIVE = 1;
+
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'lead';
+
+    public function company()
+    {
+        return $this->hasOne('App\Models\Company', 'id', 'company_id');
+    }
+
+    public function getAll()
+    {
+        return Customer::all();
+    }
+
+    public function getAllByCompanyId(int $company_id)
+    {
+        return Customer::where('company_id', $company_id)->get();
+    }
+
+}
