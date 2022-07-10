48f37f50cf9cb6975525cc2cea38cf266e8b78cd
Gustavo Novaro <gnovaro@gmail.com>
2022-07-10 15:17:39 +0200

    WIP: Contacts

---
+++ database/migrations/2022_07_10_123934_create_contact_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('lead_id')->nullable();
+            $table->bigInteger('customer_id')->nullable();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('phone', 15)->nullable();
+            $table->string('email', 254)->nullable();
+            $table->string('country', 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
--- resources/views/lead/import.blade.php
+++ resources/views/lead/import.blade.php
@@ -6,7 +6,7 @@
     </header>
 
     <div>
-        <a href="/asset/upload/import/hammer_lead_example.csv" target="_blank" class="btn btn-outline-secondary">{{ __('Download example file') }} <i class="las la-file-csv"></i></a>
+        <a href="/asset/upload/import/hammer_lead_example.csv" target="_blank" class="btn btn-outline-success">{{ __('Download example file') }} <i class="las la-file-csv"></i></a>
     </div>
 
     <form method="POST" action="/lead/save" enctype="multipart/form-data" class="form">
