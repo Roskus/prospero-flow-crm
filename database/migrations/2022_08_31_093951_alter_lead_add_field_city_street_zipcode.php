a2238db746464e3def2fa48a7a884eb4a8945388
Gustavo Novaro <gnovaro@gmail.com>
2022-08-31 11:45:57 +0200

    Add missing address fields for lead

---
+++ database/migrations/2022_08_31_093951_alter_lead_add_field_city_street_zipcode.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterLeadAddFieldCityStreetZipcode extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lead', function (Blueprint $table) {
+            $table->string('city', 50)->nullable()->after('country_id');
+            $table->string('street', 80)->nullable()->after('city');
+            $table->string('zipcode', 10)->nullable()->after('street');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lead', function (Blueprint $table) {
+            $table->dropColumn('city');
+            $table->dropColumn('street');
+            $table->dropColumn('zipcode');
+        });
+    }
+}
