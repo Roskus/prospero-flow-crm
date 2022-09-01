301329a77afc5bf03bdeab14419e623a6dc06f52
Gustavo Novaro <gnovaro@gmail.com>
2022-09-01 23:03:41 +0200

    Fix missing delete lead code

--- app/Http/Controllers/Lead/LeadDeleteController.php
+++ app/Http/Controllers/Lead/LeadDeleteController.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers\Lead;
 
+use Illuminate\Http\Request;
+use App\Models\Lead;
+
 class LeadDeleteController
 {
-
+    public function delete(Request $request, int $id)
+    {
+        $lead = Lead::find($id);
+        $lead->delete();
+        return redirect('/lead');
+    }
 }
