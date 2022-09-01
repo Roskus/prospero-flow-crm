1c17109e301b3f6fd5aba0a899e5ab34ebbc01b8
Gustavo Novaro <gnovaro@gmail.com>
2022-09-02 01:58:23 +0200

    WIP

--- database/seeders/IndustrySeeder.php
+++ database/seeders/IndustrySeeder.php
@@ -26,32 +26,49 @@ class IndustrySeeder extends Seeder
         DB::table('industry')->insert(['name' => 'Consulting']);
         DB::table('industry')->insert(['name' => 'Creative']);
         DB::table('industry')->insert(['name' => 'Culture']);
+        DB::table('industry')->insert(['name' => 'Chemical']);
 
         DB::table('industry')->insert(['name' => 'Education']);
         DB::table('industry')->insert(['name' => 'Electronics']);
         DB::table('industry')->insert(['name' => 'Energy']);
         DB::table('industry')->insert(['name' => 'Entertainment & Leisure']);
 
+        DB::table('industry')->insert(['name' => 'Finance']);
+
         DB::table('industry')->insert(['name' => 'Healthcare']);
+        DB::table('industry')->insert(['name' => 'Hospitality']);
+
+        DB::table('industry')->insert(['name' => 'Legal']);
 
         DB::table('industry')->insert(['name' => 'Real Estate']);
+        DB::table('industry')->insert(['name' => 'Shipping']);
+        DB::table('industry')->insert(['name' => 'Sports']);
+
+
         DB::table('industry')->insert(['name' => 'Venture Capital']);
         DB::table('industry')->insert(['name' => 'Water']);
 
         /*
-        <option value="Sports">Deportes</option>
-
         <option value="Manufacturing">Fabricación</option>
-        <option value="Finance">Finanzas</option>
-        <option value="Hospitality">Hostelería</option>
-        <option value="Legal">Jurídica</option>
         <option value="Marketing">Marketing</option>
         <option value="Building Materials &amp; Equipment">Materiales de construcción y equipamiento</option>
         <option value="Mass Media">Medios masivos</option>
         <option value="Mining">Minería</option>
         <option value="Music">Música</option>
-        <option value="Shipping">Naviera</option><option value="Petroleum">Petróleo</option><option value="Testing, Inspection &amp; Certification">Pruebas, inspección y certificación</option><option value="Publishing">Publicaciones</option><option value="Advertising">Publicidad</option><option value="Chemical">Química</option><option value="Apparel &amp; Accessories">Ropa y accesorios</option><option value="Insurance">Seguros</option><option value="Service">Servicio</option><option value="Software">Software</option><option value="Support">Soporte</option><option value="Technology">Tecnología</option><option value="Telecommunications">Telecomunicaciones</option><option value="Television">Televisión</option><option value="Transportation">Transporte</option><option value="Wholesale">Venta al por mayor</option><option value="Retail">Venta al por menor</option><option value="Travel">Viajes</option>
-
+        <option value="">Naviera</option>
+        <option value="Petroleum">Petróleo</option>
+        <option value="Testing, Inspection &amp; Certification">Pruebas, inspección y certificación</option>
+        <option value="Publishing">Publicaciones</option><option value="Advertising">Publicidad</option>
+        <option value="Apparel &amp; Accessories">Ropa y accesorios</option>
+        <option value="Insurance">Seguros</option>
+        <option value="Service">Servicio</option>
+        <option value="Software">Software</option>
+        <option value="Support">Soporte</option>
+        <option value="Technology">Tecnología</option>
+        <option value="Telecommunications">Telecomunicaciones</option>
+        <option value="Television">Televisión</option><option value="Transportation">Transporte</option>
+        <option value="Wholesale">Venta al por mayor</option><option value="Retail">Venta al por menor</option>
+        <option value="Travel">Viajes</option>
          */
     }
 }
