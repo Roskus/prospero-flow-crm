3d86016cb19cfebb01f0ac75345b54b68afa7f11
Gustavo Novaro <gnovaro@gmail.com>
2022-09-04 19:08:52 +0200

    WIP Supplier crud working

--- app/Http/Controllers/Supplier/SupplierSaveController.php
+++ app/Http/Controllers/Supplier/SupplierSaveController.php
@@ -2,7 +2,24 @@
 
 namespace App\Http\Controllers\Supplier;
 
-class SupplierSaveController
-{
+use App\Http\Controllers\MainController;
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
+
+class SupplierSaveController extends MainController
+{
+    public function save(Request $request)
+    {
+        $supplier = (empty($request->id)) ? new Supplier() : Supplier::find($request->id);
+        $supplier->company_id = Auth::user()->company_id;
+        $supplier->name = $request->name;
+        $supplier->phone = $request->phone;
+        $supplier->email = $request->email;
+        $supplier->website = $request->website;
+        $supplier->updated_at = now();
+        $supplier->save();
+        return redirect('/supplier');
+    }
 }
--- app/Http/Controllers/Supplier/SupplierUpdateController.php
+++ app/Http/Controllers/Supplier/SupplierUpdateController.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers\Supplier;
 
-class SupplierUpdateController
-{
+use App\Http\Controllers\MainController;
+use App\Models\Supplier;
+use Illuminate\Http\Request;
 
+class SupplierUpdateController extends MainController
+{
+    public function update(Request $request, int $id)
+    {
+        $data['supplier'] = Supplier::find($id);
+        return view('supplier.supplier', $data);
+    }
 }
