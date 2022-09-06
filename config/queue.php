0f0c4d774e3a8a2685155adfb81d5f77cffd1834
Gustavo Novaro <gnovaro@gmail.com>
2022-09-07 01:40:14 +0200

    db queue

--- config/queue.php
+++ config/queue.php
@@ -36,7 +36,7 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'table' => 'jobs',
+            'table' => 'job',
             'queue' => 'default',
             'retry_after' => 90,
         ],
