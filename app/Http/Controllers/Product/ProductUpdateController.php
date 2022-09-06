9e07ae4c618e326a5af39b742e2bc0984906c28d
Gustavo Novaro <gnovaro@gmail.com>
2022-09-07 01:32:37 +0200

    Fix missing

--- app/Http/Controllers/Product/ProductUpdateController.php
+++ app/Http/Controllers/Product/ProductUpdateController.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers\Product;
 
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Brand;
+use App\Models\Category;
+
 class ProductUpdateController
 {
-
+    public function update(Request $request, int $id)
+    {
+        $product = Product::find($id);
+        $category = new Category();
+        $brand = new Brand();
+        $data['product'] = $product;
+        $data['categories'] = $category->getAll();
+        $data['brands'] = $brand->getAll();
+        return view('product/product', $data);
+    }
 }
