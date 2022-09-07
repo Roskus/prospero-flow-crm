3d041190a65029f44a5b8fa134d002555872b1e3
Gustavo Novaro <gnovaro@gmail.com>
2022-09-07 23:05:55 +0200

    WIP: New tickets model and migration for tickets module. Upgrade composer dependencies

---
+++ database/migrations/2022_09_07_204346_create_ticket_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 80);
+            $table->text('description');
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('assigned_to')->nullable();
+            $table->enum('priority',['low', 'normal', 'high', 'urgent'])->default('normal');
+            $table->enum('type',['','question', 'incident', 'issue'])->default('');
+            $table->enum('status',['new', 'assigned', 'duplicated', 'closed'])->default('new');
+            $table->timestamps();
+            $table->dateTime('closed_at')->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket');
+    }
+}
