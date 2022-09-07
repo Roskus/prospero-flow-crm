79ab34bf97dd5a400e58614cb3f0561fae738b97
Gustavo Novaro <gnovaro@gmail.com>
2022-09-07 02:16:46 +0200

    Top menu. selected

--- resources/views/menu/top.blade.php
+++ resources/views/menu/top.blade.php
@@ -1,27 +1,47 @@
 <ul class="navbar-nav me-auto mb-2 mb-md-0">
     <li class="nav-item active">
-        <a class="nav-link fs-5" href="/"><i class="las la-compass"></i> {{ __('Dashboard') }}</a>
+        <a href="{{ url('/') }}" class="nav-link fs-5 @if(Request::path() == '/') active @endif">
+            <i class="las la-compass"></i> {{ __('Dashboard') }}
+        </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link fs-5" href="/lead"><i class="las la-filter"></i> {{ __('Leads') }}</a>
+        <a href="{{ url('/lead') }}" class="nav-link fs-5 @if(Request::path() == 'lead') active @endif">
+            <i class="las la-filter"></i> {{ __('Leads') }}
+        </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link fs-5" href="/customer"><i class="las la-user-alt"></i> {{ __('Customers') }}</a>
+        <a href="{{ url('/customer') }}" class="nav-link fs-5 @if(Request::path() == 'customer') active @endif">
+            <i class="las la-user-alt"></i> {{ __('Customers') }}
+        </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link fs-5" href="/order"><i class="las la-receipt"></i> {{ __('Orders') }}</a>
+        <a href="{{ url('/order') }}" class="nav-link fs-5 @if(Request::path() == 'order') active @endif">
+            <i class="las la-receipt"></i> {{ __('Orders') }}
+        </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link fs-5" href="/product"><i class="las la-box"></i> {{ __('Products') }}</a>
+        <a href="{{ url('/product') }}" class="nav-link fs-5 @if(Request::path() == 'product') active @endif">
+            <i class="las la-box"></i> {{ __('Products') }}
+        </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link fs-5" href="/calendar"><i class="las la-calendar"></i> {{ __('Calendar') }}</a>
+        <a href="{{ url('/calendar') }}" class="nav-link fs-5 @if(Request::path() == 'calendar') active @endif">
+            <i class="las la-calendar"></i> {{ __('Calendar') }}
+        </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link fs-5" href="/email"><i class="las la-envelope-open-text"></i> E-mail</a>
+        <a href="{{ url('/email') }}" class="nav-link @if(Request::path() == 'email') active @endif fs-5">
+            <i class="las la-envelope-open-text"></i> E-mail
+        </a>
     </li>
     <li class="nav-item fs-5">
-        <a class="nav-link" href="/accounting"><i class="las la-coins"></i> {{ __('Accounting') }}
+        <a href="{{ url('/accounting') }}" class="nav-link @if(Request::path() == 'accounting') active @endif">
+            <i class="las la-coins"></i> {{ __('Accounting') }}
+        </a>
+    </li>
+    <li class="nav-item fs-5">
+        <a href="{{ url('/supplier') }}" class="nav-link @if(Request::path() == 'supplier') active @endif" title="{{ __('Suppliers') }}">
+            <i class="las la-dolly"></i> {{ __('Suppliers') }}
         </a>
     </li>
 </ul>
