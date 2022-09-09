411b2d16d215d080e9425b93a744aa092a4655e2
Gustavo Novaro <gnovaro@gmail.com>
2022-09-09 16:13:36 +0200

    Fix Product create

--- app/Http/Controllers/Product/ProductCreateController.php
+++ app/Http/Controllers/Product/ProductCreateController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Product;
 
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Brand;
 use App\Models\Category;
@@ -15,6 +15,7 @@ class ProductCreateController
         $product = new Product();
         $category = new Category();
         $brand = new Brand();
+        $data['product'] = $product;
         $data['products'] = $product->getAllByCompanyId(Auth::user()->company_id);
         $data['categories'] = $category->getAllActiveByCompany(Auth::user()->company_id);
         $data['brands'] = $brand->getAllActiveByCompany(Auth::user()->company_id);
