2790d9b8fefff8c97c08069508a9e1192638f95a
Gustavo Novaro <gnovaro@gmail.com>
2022-09-20 13:05:24 +0200

    Improve database stucture. Add user->company missing foreign. Create calendar table

---
+++ database/migrations/2022_09_20_103926_alter_table_user_primary2bigint.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->change();
+            $table->foreign('company_id')->on('company')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->dropIndex('company_id');
+        });
+    }
+};
---
+++ database/migrations/2022_09_20_104327_create_calendar_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date')->nullable();
+            $table->boolean('is_all_day')->nullable();
+            $table->string('title', 80);
+            $table->text('description')->nullable();
+            $table->json('guests')->nullable();
+            $table->string('meeting', 255)->nullable(); //Web link for meeting Google Meet, Jitsi, Zoom
+            $table->string('address', 255)->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('company_id')->on('company')->references('id');
+            $table->foreign('user_id')->on('user')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar');
+    }
+};
