f239a737798cbb76a9df47ce6fbf7243eeaa5ec5
Gustavo Novaro <gnovaro@gmail.com>
2022-09-26 17:16:36 +0200

    Fix API Doc json generator

--- app/Http/Controllers/Api/Doc/DocGeneratorController.php
+++ app/Http/Controllers/Api/Doc/DocGeneratorController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api\Doc;
 
+use Illuminate\Http\JsonResponse;
+
 /**
  * @OA\Info(title="Hammer CRM API", version="1.0")
  */
@@ -10,16 +12,15 @@ class DocGeneratorController
     /**
      * @OA\Get(
      *     path="/resource.json",
-     *     @OA\Response(response="200", description="An example resource")
+     *     @OA\Response(response="200", description="API JSON Specification OpenAPI format")
      * )
      */
-    public function render()
+    public function render() : JsonResponse
     {
-        $app_path = base_path();
-        $openapi = OpenApi\Generator::scan([
+        $app_path = app_path();
+        $openapi = \OpenApi\Generator::scan([
             $app_path.DIRECTORY_SEPARATOR.'Http'.DIRECTORY_SEPARATOR.'Controllers'.DIRECTORY_SEPARATOR.'Api',
         ]);
-
         return response()->json($openapi->toJson());
     }
 }
