cd92363d770a94e1a8701084902ae918c9fd19dd
gnovaro <gnovaro@users.noreply.github.com>
2022-09-28 19:10:40 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/User/UserListController.php
+++ app/Http/Controllers/User/UserListController.php
@@ -4,5 +4,4 @@ namespace App\Http\Controllers\User;
 
 class UserListController
 {
-
 }
--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
 //use Illuminate\Database\Eloquent\SoftDeletes;
 use OpenApi\Annotations\OpenApi as OA;
 
