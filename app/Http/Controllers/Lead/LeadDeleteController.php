85833337b0bb05d7f312bf66cb61e5b51b2dd2e0
gnovaro <gnovaro@users.noreply.github.com>
2022-09-30 17:08:12 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/Lead/LeadDeleteController.php
+++ app/Http/Controllers/Lead/LeadDeleteController.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 class LeadDeleteController extends MainController
 {
     /**
-     * @param Request $request
-     * @param int $id
+     * @param  Request  $request
+     * @param  int  $id
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function delete(Request $request, int $id)
