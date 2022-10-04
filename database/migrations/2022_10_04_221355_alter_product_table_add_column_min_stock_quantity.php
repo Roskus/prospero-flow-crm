e4ec290297cbbc106fa4d20c270e8f5b4fd2f57c
Gustavo Novaro <gnovaro@gmail.com>
2022-10-05 00:34:43 +0200

    Missing files

---
+++ database/migrations/2022_10_04_221355_alter_product_table_add_column_min_stock_quantity.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('product', function (Blueprint $table) {
+            $table->integer('min_stock_quantity')->nullable()->default(0)->after('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product', function (Blueprint $table) {
+            $table->dropColumn('min_stock_quantity');
+        });
+    }
+};
