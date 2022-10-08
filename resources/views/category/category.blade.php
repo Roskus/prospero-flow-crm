9ca081fa17dc17dd63aa3fa4dbbdceb03bfa6189
Gustavo Novaro <gnovaro@users.noreply.github.com>
2022-10-09 01:25:50 +0200

    Prepare validation

--- resources/views/category/category.blade.php
+++ resources/views/category/category.blade.php
@@ -5,16 +5,16 @@
    <h1>{{ __('Category') }}</h1>
 </header>
 
-<form method="post" action="/category/save">
+<form method="post" action="/category/save">    
     <div class="form-group">
         <label class="label-control">{{ __('Name') }} <span class="text-danger">*</span></label>
-        <input type="text" name="name" id="name" value="{{ $category->name }}" class="form-control" required="required">
+        <input type="text" name="name" id="name" value="{{ @old('name', $category->name) }}" class="form-control" required="required">
     </div>
     <div class="form-group mt-2">
         <button type="submit" class="btn btn-primary"><span class=""></span> {{ __('Save') }}</button>
     </div>
     <input type="hidden" name="id" id="id" value="{{ $category->id }}">
-    <input type="hidden" name="company_id" id="company_id" value="">
+    <input type="hidden" name="company_id" id="company_id" value="{{ Auth::user()->company_id }}">
     {{ csrf_field() }}
 </form>
 
