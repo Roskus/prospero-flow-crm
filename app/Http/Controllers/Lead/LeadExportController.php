1747c3ce49001f5d393ae11407c81cd55afce70c
gnovaro <gnovaro@users.noreply.github.com>
2022-10-09 01:31:10 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/Lead/LeadExportController.php
+++ app/Http/Controllers/Lead/LeadExportController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Lead;
 
 use App\Http\Controllers\MainController;
-use Illuminate\Http\Request;
 use App\Models\Lead;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
@@ -15,22 +15,21 @@ class LeadExportController extends MainController
         $content = '';
         $fileName = 'leads_'.Auth::user()->company->name.'_'.date('Ymd_His').'.csv';
         $leads = Lead::where('company_id', Auth::user()->company_id)->get();
-        $headers = array(
-            "Content-type"        => "text/csv",
-            "Content-Disposition" => "attachment; filename=$fileName",
-            "Pragma"              => "no-cache",
-            "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
-            "Expires"             => "0"
-        );
+        $headers = [
+            'Content-type' => 'text/csv',
+            'Content-Disposition' => "attachment; filename=$fileName",
+            'Pragma' => 'no-cache',
+            'Cache-Control' => 'must-revalidate, post-check=0, pre-check=0',
+            'Expires' => '0',
+        ];
         $data = $leads->toArray();
 
-        foreach ($data as $row)
-        {
+        foreach ($data as $row) {
             $content = $row.var_export($row, true);
         }
 
         Storage::disk('local')->put(storage_path($fileName), $content);
+
         return Storage::download(storage_path($fileName));
     }
-
 }
