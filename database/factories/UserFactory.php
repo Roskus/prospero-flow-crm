cf388bb2e7ca5c9a463f6e0be1518c0f4014c716
Gustavo Novaro <gnovaro@gmail.com>
2022-10-09 14:37:55 +0200

    Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,7 +18,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            //
+             'first_name' => $this->faker->firstName(),
+             'last_name' => $this->faker->lastName(),
+             'email' => $this->faker->unique()->safeEmail(),
+             'email_verified_at' => now(),
+             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+             'remember_token' => Str::random(10),
         ];
     }
 }
