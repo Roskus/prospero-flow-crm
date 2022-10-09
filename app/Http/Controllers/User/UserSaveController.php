5d4c5105fffca0d495149264c1fa221da26ff1bb
gnovaro <gnovaro@users.noreply.github.com>
2022-10-09 17:08:45 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/User/UserSaveController.php
+++ app/Http/Controllers/User/UserSaveController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Spatie\Permission\Models\Role;
 
 class UserSaveController extends MainController
 {
