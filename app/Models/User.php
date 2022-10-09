fa3efa86d733ebd6c82e31b95b331d0fcd7f4f1d
gnovaro <gnovaro@users.noreply.github.com>
2022-10-09 12:26:57 +0000

    PHP Linting (Pint)

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
-use App\Models\User;
 
 class UserSeeder extends Seeder
 {
