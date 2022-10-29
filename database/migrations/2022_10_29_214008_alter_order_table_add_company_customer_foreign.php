127528361eb431c38a4333d8a6afddef4d5a90e9
Gustavo Novaro <gnovaro@gmail.com>
2022-10-29 23:50:10 +0200

    Improve database relation. Order foreign keys

---
+++ database/migrations/2022_10_29_214008_alter_order_table_add_company_customer_foreign.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->foreign('company_id')->references('id')->on('company');
+            $table->foreign('customer_id')->references('id')->on('customer');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->dropForeign(['company_id']);
+            $table->dropForeign(['customer_id']);
+        });
+    }
+};
