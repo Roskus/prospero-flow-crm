9799d0be066a64c0b44227a72a6ea25839dfe760
Gustavo Novaro <gnovaro@gmail.com>
2022-11-08 02:54:30 +0100

    Add meta header for manifest in the app loyout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
     <meta name="googlebot" content="noindex">
     <title>{{ isset($title) ? $title.' | Hammer CRM' : 'Hammer CRM' }}</title>
     <link rel="icon" href="/favicon.png" type="image/x-icon">
+    <link rel="manifest" href="{{ url('/manifest') }}">
+
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
