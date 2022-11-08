eae11773abc486243fead0b32474a21892b1b95a
Gustavo Novaro <gnovaro@gmail.com>
2022-11-08 02:52:00 +0100

    PWA Progresive Web Application manifest

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'MainController@index');
+// PWA - Progresive Web App
+Route::get('/manifest', [\App\Http\Controllers\ManifestController::class, 'renderWebManifest'])->name('manifest');
 
 Auth::routes(['register' => env('APP_ENV') != 'production']);
 
