f7ab8da862b49ebef7531ec7ad0663906c6e7c63
Gustavo Novaro <gnovaro@gmail.com>
2022-11-08 17:31:56 +0100

    Sidebar menu selected

--- resources/views/menu/sidebar.blade.php
+++ resources/views/menu/sidebar.blade.php
@@ -11,52 +11,52 @@
             </a>
         </li>
         <li>
-            <a href="{{ url('/lead') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'lead') active @endif" title="{{ __('Leads') }}" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/lead') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(),'lead')) active @endif" title="{{ __('Leads') }}" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-filter fs-1"></i>
             </a>
         </li>
         <li>
-            <a href="{{ url('/customer') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'customer') active @endif" title="{{ __('Customers') }}" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/customer') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'customer')) active @endif" title="{{ __('Customers') }}" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-user-alt fs-1"></i>
             </a>
         </li>
         <li>
-            <a href="{{ url('/order') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'order') active @endif" title="{{ __('Orders') }}" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/order') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'order')) active @endif" title="{{ __('Orders') }}" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-receipt fs-1"></i>
             </a>
         </li>
         <li>
-            <a href="{{ url('/product') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'product') active @endif" title="{{ __('Products') }}" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/product') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'product')) active @endif" title="{{ __('Products') }}" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-box fs-1"></i>
             </a>
         </li>
         <li>
-            <a href="{{ url('/calendar') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'calendar') active @endif" title="{{ __('Calendar') }}" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/calendar') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'calendar')) active @endif" title="{{ __('Calendar') }}" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-calendar fs-1"></i>
             </a>
         </li>
         <li>
-            <a href="{{ url('/email') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'email') active @endif" title="E-mail" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/email') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'email')) active @endif" title="E-mail" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-envelope-open-text fs-1"></i>
             </a>
         </li>
         <li>
-            <a href="{{ url('/accounting') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'accounting') active @endif" title="{{ __('Accounting') }}" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/accounting') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'accounting')) active @endif" title="{{ __('Accounting') }}" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-coins fs-1"></i>
             </a>
         </li>
         <li>
-            <a href="{{ url('/supplier') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'supplier') active @endif" title="{{ __('Suppliers') }}" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/supplier') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(),'supplier')) active @endif" title="{{ __('Suppliers') }}" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-dolly fs-1"></i>
             </a>
         </li>
         <li class="nav-item fs-5">
-            <a href="{{ url('/ticket') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'ticket') active @endif" title="{{ __('Tickets') }}" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/ticket') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'ticket')) active @endif" title="{{ __('Tickets') }}" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-tools fs-1"></i>
             </a>
         </li>
         <li class="nav-item fs-5">
-            <a href="{{ url('/report') }}" class="nav-link py-2 border-bottom @if(Request::path() == 'report') active @endif" title="{{ __('Reports') }}" data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="{{ url('/report') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'report')) active @endif" title="{{ __('Reports') }}" data-bs-toggle="tooltip" data-bs-placement="right">
                 <i class="las la-chart-pie fs-1"></i>
             </a>
         </li>
