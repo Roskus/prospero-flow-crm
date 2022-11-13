2ac302236a6d488f7a2148f9578f375c08937f99
gnovaro <gnovaro@users.noreply.github.com>
2022-11-13 21:35:29 +0000

    PHP Linting (Pint)

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 //use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Campaign extends Model
