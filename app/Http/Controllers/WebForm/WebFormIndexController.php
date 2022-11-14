5e84b92f0b287d8ede21bca658d2aedfc8cbccf0
Gustavo Novaro <gnovaro@users.noreply.github.com>
2022-11-14 16:44:40 +0100

    WIP Webforms

--- app/Http/Controllers/WebForm/WebFormIndexController.php
+++ app/Http/Controllers/WebForm/WebFormIndexController.php
@@ -9,6 +9,7 @@ class WebFormIndexController extends MainController
 {
     public function index(Request $request)
     {
-        return view('web_form.index');
+        $data['webforms'] = [];
+        return view('web_form.index', $data);
     }
 }
