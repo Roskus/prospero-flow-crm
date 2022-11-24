5572e9b63e2a69f6301efed4c3cd12a6805b8248
Gustavo Novaro <gnovaro@gmail.com>
2022-11-25 00:14:29 +0100

    Import customer

--- resources/views/customer/import.blade.php
+++ resources/views/customer/import.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
     <header>
-        <h1>{{ __('Lead import') }}
+        <h1>{{ __('Customer import') }}
     </header>
 
     <div>
         <a href="/asset/upload/import/hammer_lead_example.csv" target="_blank" class="btn btn-outline-success">{{ __('Download example file') }} <i class="las la-file-csv"></i></a>
     </div>
 
-    <form method="POST" action="{{ url('/lead/import/save') }}" enctype="multipart/form-data" class="form">
+    <form method="POST" action="{{ url('/customer/import/save') }}" enctype="multipart/form-data" class="form">
         @csrf
         <div class="row">
-            <label>{{ __('File') }}</label>
+            <label for="upload">{{ __('File') }}</label>
             <input type="file" name="upload" accept="text/csv" required>
         </div>
         <div class="row mt-2">
             <div class="col">
-                <a href="{{ url('/lead') }}" class="btn btn-secondary">{{ __('Cancel') }}</a>
+                <a href="{{ url('/customer') }}" class="btn btn-secondary">{{ __('Cancel') }}</a>
                 <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
             </div>
         </div>
