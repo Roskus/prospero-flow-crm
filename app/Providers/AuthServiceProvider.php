6d76cb09550ab86ca605b981c80de24bde399043
gnovaro <gnovaro@users.noreply.github.com>
2022-11-25 17:48:36 +0000

    PHP Linting (Pint)

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace App\Providers;
 
-use Exception;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Throwable;
 
 class AuthServiceProvider extends ServiceProvider
 {
