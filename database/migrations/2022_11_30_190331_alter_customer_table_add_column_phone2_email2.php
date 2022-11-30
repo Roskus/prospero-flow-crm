988e2b8e985eb76dff5c519652a294e1f9cfbbe6
Gustavo Novaro <gnovaro@gmail.com>
2022-11-30 22:34:51 +0100

    New fields

---
+++ database/migrations/2022_11_30_190331_alter_customer_table_add_column_phone2_email2.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            $table->string('phone2', 15)->nullable()->after('phone');
+            $table->string('email2', 254)->nullable()->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            $table->dropColumn('phone2');
+            $table->dropColumn('email2');
+        });
+    }
+};
