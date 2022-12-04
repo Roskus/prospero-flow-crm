683bffc37f5a64a3a19e7c68e266ed2d0b6eb521
Antonio <elguitarraverde@hotmail.com>
2022-12-04 11:46:47 +0100

    feat: :rocket: Calendar event delete

--- routes/web.php
+++ routes/web.php
@@ -102,6 +102,7 @@ Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index']);
 Route::match(['get', 'post'], '/calendar/{date?}', [\App\Http\Controllers\Calendar\CalendarController::class, 'index'])->name('calendar.index');
 Route::post('/calendar/event/save', [\App\Http\Controllers\Calendar\SaveCalendarEventController::class, 'save'])->name('calendar.save');
 Route::get('/calendar/event/update/{id}', [\App\Http\Controllers\Calendar\UpdateCalendarEventController::class, 'update'])->name('calendar.update');
+Route::delete('/calendar/event/delete/{id}', [\App\Http\Controllers\Calendar\DeleteCalendarEventController::class, 'delete'])->name('calendar.delete');
 
 // Email
 Route::match(['get', 'post'], '/email', [\App\Http\Controllers\Email\EmailIndexController::class, 'index']);
