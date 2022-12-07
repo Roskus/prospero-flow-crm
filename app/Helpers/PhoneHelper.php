90a30f31a2e111fa351f1a89e2d872d7f63bb7f9
gnovaro <gnovaro@users.noreply.github.com>
2022-12-07 01:36:34 +0000

    PHP Linting (Pint)

--- app/Helpers/PhoneHelper.php
+++ app/Helpers/PhoneHelper.php
@@ -5,20 +5,20 @@ namespace App\Helpers;
 class PhoneHelper
 {
     /**
-     * @param string $phone
+     * @param  string  $phone
      * @return string
      */
-    public static function format(string $phone) : string
+    public static function format(string $phone): string
     {
         $length = strlen($phone);
-        switch ($length)
-        {
+        switch ($length) {
             case 12:
-                $format = substr($phone, 0, 3).'-'.substr($phone, 3, 3).'-'.substr($phone,6,3).'-'.substr($phone,9,3);
+                $format = substr($phone, 0, 3).'-'.substr($phone, 3, 3).'-'.substr($phone, 6, 3).'-'.substr($phone, 9, 3);
                 break;
             default:
                 $format = $phone;
         }
+
         return $format;
     }
 }
