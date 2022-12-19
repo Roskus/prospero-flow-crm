f41eb6c9a039d278ee5b21e2390b714b5754b484
gnovaro <gnovaro@users.noreply.github.com>
2022-12-19 22:13:22 +0000

    PHP Linting (Pint)

--- database/seeders/BankSeeder.php
+++ database/seeders/BankSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,25 +15,23 @@ class BankSeeder extends Seeder
      */
     public function run()
     {
-
         //Albania
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'Alpha Bank (Albania)', 'website' => 'https://alphabank.al']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'American Bank of Investments (ABI)', 'website' => 'https://abi.al']);
-        DB::table('bank')->insert(['country'=> 'al','name' => 'BKT', 'website' => 'https://bkt.com.al']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'Credins Bank (CB)', 'website' => 'https://bankacredins.com']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'Fibank', 'website' => 'https://fibank.al']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'Intesa Sanpaolo Bank (Albania)', 'website' => 'https://intesasanpaolobank.al']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'ProCredit Bank', 'website' => 'https://procreditbank.com.al']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'Raiffeisen (Albania)', 'website' => 'https://raiffeisen.al']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'OTP Bank', 'website' => 'https://otpbank.al']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'Tirana Bank', 'website' => 'https://tiranabank.al']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'Union Bank (Albania)', 'website' => 'https://unionbank.al']);
-        DB::table('bank')->insert(['country'=> 'al', 'name' => 'United Bank of Albania', 'website' => 'https://uba.com.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'Alpha Bank (Albania)', 'website' => 'https://alphabank.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'American Bank of Investments (ABI)', 'website' => 'https://abi.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'BKT', 'website' => 'https://bkt.com.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'Credins Bank (CB)', 'website' => 'https://bankacredins.com']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'Fibank', 'website' => 'https://fibank.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'Intesa Sanpaolo Bank (Albania)', 'website' => 'https://intesasanpaolobank.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'ProCredit Bank', 'website' => 'https://procreditbank.com.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'Raiffeisen (Albania)', 'website' => 'https://raiffeisen.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'OTP Bank', 'website' => 'https://otpbank.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'Tirana Bank', 'website' => 'https://tiranabank.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'Union Bank (Albania)', 'website' => 'https://unionbank.al']);
+        DB::table('bank')->insert(['country' => 'al', 'name' => 'United Bank of Albania', 'website' => 'https://uba.com.al']);
 
         //Andorra
-        DB::table('bank')->insert(['country'=> 'ad', 'name' => 'Andbank', 'phone' => '+376739011', 'email' => 'info@andbank.com', 'website' => 'https://www.andbank.com']);
-        DB::table('bank')->insert(['country'=> 'ad', 'name' => 'Crèdit Andorrà, SA', 'phone' => '+376888888', 'email' => 'info@creditandorra.ad', 'website' => 'https://comercial.creditandorragroup.ad/en']);
-        DB::table('bank')->insert(['country'=> 'ad', 'name' => 'Mora Banc Grup, SA', 'phone' => '+376884884', 'email' => 'morabanc@morabanc.ad', 'website' => 'https://www.morabanc.ad/']);
-
+        DB::table('bank')->insert(['country' => 'ad', 'name' => 'Andbank', 'phone' => '+376739011', 'email' => 'info@andbank.com', 'website' => 'https://www.andbank.com']);
+        DB::table('bank')->insert(['country' => 'ad', 'name' => 'Crèdit Andorrà, SA', 'phone' => '+376888888', 'email' => 'info@creditandorra.ad', 'website' => 'https://comercial.creditandorragroup.ad/en']);
+        DB::table('bank')->insert(['country' => 'ad', 'name' => 'Mora Banc Grup, SA', 'phone' => '+376884884', 'email' => 'morabanc@morabanc.ad', 'website' => 'https://www.morabanc.ad/']);
     }
 }
