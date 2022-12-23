161375adf2223dc13fdd4c068bad92b7abad821f
Antonio <elguitarraverde@hotmail.com>
2022-12-23 19:41:05 +0100

    test: add ProfileSaveControllerTest

---
+++ tests/Feature/Controllers/Profile/ProfileSaveControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature\Controllers\Profile;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProfileSaveControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_save_profile()
+    {
+        $user = $this->signIn();
+        $this->actingAs($user);
+
+        $response = $this->post('profile/save', []);
+        $response->assertSessionHasErrors();
+
+        $data = [
+            'first_name' => fake()->name(), 
+            'last_name' => fake()->name(), 
+            'email' => fake()->email(),
+            'lang' => fake()->randomElement(array_keys(config('app.locales'))),
+        ];
+        $response = $this->post('profile/save', $data);
+        $response->assertRedirect('/profile');
+
+        $response = $this->get('/profile');
+        $response->assertSee($data['first_name']);
+        $response->assertSee($data['last_name']);
+        $response->assertSee($data['email']);
+
+        // UPDATE PASSWORD
+        $oldPassword = $user->password;
+        $newPassword = fake()->password(8);
+        $data = [
+            'first_name' => fake()->name(), 
+            'last_name' => fake()->name(), 
+            'email' => fake()->email(),
+            'lang' => fake()->randomElement(array_keys(config('app.locales'))),
+            'password' => $newPassword,
+            'password_confirmation' => $newPassword,
+        ];
+        $response = $this->post('profile/save', $data);
+        
+        $this->assertNotEquals($oldPassword, User::first()->password);
+    }
+}
