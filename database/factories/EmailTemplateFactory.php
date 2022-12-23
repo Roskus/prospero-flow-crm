825493e4379da818e53a3c583135102b1fc078c8
Antonio <elguitarraverde@hotmail.com>
2022-12-23 20:51:29 +0100

    test: add EmailIndexControllerTest

---
+++ database/factories/EmailTemplateFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EmailTemplate>
+ */
+class EmailTemplateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'from' => $this->faker->email(),
+            'subject' => $this->faker->title(),
+            'body' => $this->faker->text(),
+            'lang' => fake()->randomElement(array_keys(config('app.locales'))),
+            'version' => 1,
+        ];
+    }
+}
---
+++ tests/Feature/Controllers/Email/EmailIndexControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Controllers\Email;
+
+use App\Models\Company;
+use App\Models\Email;
+use App\Models\EmailTemplate;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EmailIndexControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_show_emails_index()
+    {
+        $user = $this->signIn();
+        $this->actingAs($user);
+
+        EmailTemplate::factory()->create();
+        $email = Email::factory()->create();
+        $email2 = Email::factory()->create();
+
+        $response = $this->get('/email');
+        $response->assertStatus(200);
+        $response->assertSee($email->subject);
+        $response->assertSee($email->to);
+        $response->assertSee($email2->subject);
+        $response->assertSee($email2->to);
+
+        $response = $this->get('/email?search=' . $email->subject);
+        $response->assertStatus(200);
+        $response->assertSee($email->subject);
+        $response->assertSee($email->to);
+        $response->assertDontSee($email2->subject);
+        $response->assertDontSee($email2->to);
+    }
+}
