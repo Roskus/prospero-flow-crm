6cd8b3e931ca33ed17292335459938330f486d3a
Antonio <elguitarraverde@hotmail.com>
2023-01-02 17:28:42 +0100

    test: add EmailIndexControllerTest (#63)
    * test: add EmailIndexControllerTest
    
    * PHP Linting (Pint)
    
    * test: adding search_emails test to a separate logic
    
    Co-authored-by: elguitarraverde <elguitarraverde@users.noreply.github.com>

---
+++ database/factories/EmailFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use App\Models\EmailTemplate;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Email>
+ */
+class EmailFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'company_id' => Company::all()->random()->id,
+            'template_id' => EmailTemplate::all()->random()->id,
+            'version' => 1,
+            'from' => $this->faker->email(),
+            'to' => $this->faker->email(),
+            'cc' => $this->faker->email(),
+            'subject' => $this->faker->title(),
+            'body' => $this->faker->text(),
+            'lang' => $this->faker->randomElement(array_keys(config('app.locales'))),
+            'status' => $this->faker->randomElement(['draft', 'queue', 'sent', 'error']),
+            'schedule_send' => now(),
+        ];
+    }
+}
