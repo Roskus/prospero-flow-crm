e087f719c9128f15193b219406a6fa78368c76f6
Antonio <elguitarraverde@hotmail.com>
2023-01-14 19:28:50 +0100

    feat: configure authentication with tymon/jwt-auth package

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 /**
  *  @OA\Schema(
@@ -56,7 +57,7 @@ use Spatie\Permission\Traits\HasRoles;
  *    )
  *  )
  */
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens;
     use HasFactory;
@@ -125,5 +126,25 @@ class User extends Authenticatable
         return Attribute::make(
             get: fn ($value) => $value ?? config('app.timezone'),
         );
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
