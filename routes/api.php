8252211a592a73290f2c1992649e288d6ac9d398
Antonio <elguitarraverde@hotmail.com>
2023-01-17 01:00:50 +0100

    refactor: routes api refactor

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\LoginController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,32 +14,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/** PROTECTED ROUTES */
+Route::middleware(['api'])->group(function () {
+    // Lead
+    Route::post('lead', [\App\Http\Controllers\Api\Lead\LeadCreateController::class, 'create'])->middleware(['auth:api']);
+    Route::get('lead/{id}', [\App\Http\Controllers\Api\Lead\LeadReadController::class, 'read'])->middleware(['auth:api']);
+    Route::get('lead', [\App\Http\Controllers\Api\Lead\LeadListController::class, 'index'])->middleware(['auth:api']);
+
+    // Customer
+    //Route::get('/customers', 'Api\Customer\CustomerListController@index');
+
+    // Product
+    //Route::get('/products', 'Api\Product\ProductListController@index');
+
+    // Contact
+    Route::post('/contact', [\App\Http\Controllers\Api\Contact\ContactCreateController::class, 'create'])->middleware(['auth:api']);
+    //Route::get('/contact', 'Api\Contact\ContactListController@index');
+    //Route::patch('/contact/{id}', 'Api\Contact\ContactUpdateController@update');
+
+    /** AUTH ROUTES */
+    Route::prefix('auth')->group(function () {
+        Route::post('logout', [LoginController::class, 'logout']);
+        Route::post('refresh', [LoginController::class, 'refresh']);
+        Route::post('me', [LoginController::class, 'me']);
+    });
 });
+
+/** UNPROTECTED ROUTES */
+
+// Login
+Route::post('/auth/login', [LoginController::class, 'login']);
 
 // OpenApi Json Resource
 Route::get('/resource.json', [\App\Http\Controllers\Api\Doc\DocGeneratorController::class, 'render']);
-
-// Lead
-Route::post('lead', [\App\Http\Controllers\Api\Lead\LeadCreateController::class, 'create'])->middleware(['auth:api']);
-Route::get('lead/{id}', [\App\Http\Controllers\Api\Lead\LeadReadController::class, 'read'])->middleware(['auth:api']);
-Route::get('lead', [\App\Http\Controllers\Api\Lead\LeadListController::class, 'index'])->middleware(['auth:api']);
-
-// Customer
-//Route::get('/customers', 'Api\Customer\CustomerListController@index');
-
-// Product
-//Route::get('/products', 'Api\Product\ProductListController@index');
-
-// Contact
-Route::post('/contact', [\App\Http\Controllers\Api\Contact\ContactCreateController::class, 'create'])->middleware(['auth:api']);
-//Route::get('/contact', 'Api\Contact\ContactListController@index');
-//Route::patch('/contact/{id}', 'Api\Contact\ContactUpdateController@update');
-
-Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
-    Route::post('login', [LoginController::class, 'login']);
-    Route::post('logout', [LoginController::class, 'logout']);
-    Route::post('refresh', [LoginController::class, 'refresh']);
-    Route::post('me', [LoginController::class, 'me']);
-});
