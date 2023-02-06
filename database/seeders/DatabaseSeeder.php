9140bf2ec2361326f6665667dfb048af6d34a69a
Gustavo Novaro <gnovaro@users.noreply.github.com>
2023-02-06 21:28:46 +0100

    Rollback seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,21 +18,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        switch (App::environment()) {
-            case 'local':
-                $this->call(CompanyTableSeeder::class);
-                $this->call(RoleSeeder::class);
-                $this->call(UserSeeder::class);
-                $this->call(IndustrySeeder::class);
-                $this->call(BankSeeder::class);
-                $this->call(TicketSeeder::class);
-                break;
-            case 'testing':
-                //
-                break;
-            case 'production':
-                //
-                break;
-        }
+        $this->call(CompanyTableSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(IndustrySeeder::class);
+        $this->call(BankSeeder::class);
+        $this->call(TicketSeeder::class);
     }
 }
