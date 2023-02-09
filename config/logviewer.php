529d4890b2f7a6fcb026049400f6c2b53f0f3bd9
elguitarraverde <elguitarraverde@users.noreply.github.com>
2023-02-09 18:51:11 +0000

    PHP Linting (Pint)

--- config/logviewer.php
+++ config/logviewer.php
@@ -10,6 +10,6 @@ return [
     |
     */
     'max_file_size' => 52428800, // size in Byte
-    'pattern'       => env('LOGVIEWER_PATTERN', '*.log'),
-    'storage_path'  => env('LOGVIEWER_STORAGE_PATH', storage_path('logs')),
+    'pattern' => env('LOGVIEWER_PATTERN', '*.log'),
+    'storage_path' => env('LOGVIEWER_STORAGE_PATH', storage_path('logs')),
 ];
