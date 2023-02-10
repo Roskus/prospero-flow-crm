906a879db1ea0f49f095c1970dc1c15a11fb5d2b
Gustavo Novaro <gnovaro@gmail.com>
2023-02-10 17:33:55 +0100

    Company new fields. Improve company look & feel

---
+++ database/migrations/2023_02_10_162129_alter_company_table_add_business_name_vat_address_signature_columns.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('company', function (Blueprint $table) {
+            $table->string('business_name', 80)->nullable()->after('name');
+            $table->string('vat', 20)->nullable()->after('business_name');
+            $table->string('province', 50)->nullable()->after('country_id');
+            $table->string('city', 50)->nullable()->after('province');
+            $table->string('street', 80)->nullable()->after('city');
+            $table->string('zipcode', 10)->nullable()->after('street');
+            $table->text('signature_html')->nullable()->after('logo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('company', function (Blueprint $table) {
+            $table->dropColumn('business_name');
+            $table->dropColumn('vat');
+            $table->dropColumn('province');
+            $table->dropColumn('city');
+            $table->dropColumn('street');
+            $table->dropColumn('zipcode');
+            $table->dropColumn('signature_html');
+        });
+    }
+};
--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -8,43 +8,46 @@
 <div class="mb-2">
     <a href="{{ url("/company/create") }}" class="btn btn-primary">{{ __('New') }}</a>
 </div>
-
-<div class="table-responsive">
-    <table class="table table-bordered table-striped table-hover">
-    <thead>
-    <tr>
-        <th>{{ __('Name') }}</th>
-        <th>{{ __('Phone') }}</th>
-        <th>E-mail</th>
-        <th>{{ __('Website') }}</th>
-        <th>{{ __('Country') }}</th>
-        <th>{{ __('Created at') }}</th>
-        <th>{{ __('Updated at') }}</th>
-        <th>{{ __('Actions') }}</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($companies as $company)
-    <tr>
-        <td>
-            <a href="{{ url("/company/update/$company->id") }}">
-            {{ $company->name }}
-            </a>
-        </td>
-        <td>{{ $company->phone }}</td>
-        <td>{{ $company->email }}</td>
-        <td>
-            <a href="{{ $company->website }}" target="_blank">{{ $company->website }}</a>
-        </td>
-        <td class="text-center"><span title="{{ (!empty($company->country)) ? $company->country->name : '' }}">{{ (!empty($company->country)) ? $company->country->flag : '' }}</span></td>
-        <td>{{ $company->created_at }}</td>
-        <td>{{ $company->updated_at }}</td>
-        <td>
-            <a href="{{ url("/company/delete/$company->id") }}" class="btn btn-danger" title="{{ __('Delete') }}"><i class="las la-trash-alt"></i></a>
-        </td>
-    </tr>
-    @endforeach
-    </tbody>
-    </table>
-</div>
+<div class="card">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped table-hover">
+            <thead>
+            <tr>
+                <th>{{ __('Name') }}</th>
+                <th>{{ __('Phone') }}</th>
+                <th>E-mail</th>
+                <th>{{ __('Website') }}</th>
+                <th>{{ __('Country') }}</th>
+                <th>{{ __('Created at') }}</th>
+                <th>{{ __('Updated at') }}</th>
+                <th>{{ __('Actions') }}</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($companies as $company)
+            <tr>
+                <td>
+                    <a href="{{ url("/company/update/$company->id") }}">
+                    {{ $company->name }}
+                    </a>
+                </td>
+                <td>{{ $company->phone }}</td>
+                <td>{{ $company->email }}</td>
+                <td>
+                    <a href="{{ $company->website }}" target="_blank">{{ $company->website }}</a>
+                </td>
+                <td class="text-center"><span title="{{ (!empty($company->country)) ? $company->country->name : '' }}">{{ (!empty($company->country)) ? $company->country->flag : '' }}</span></td>
+                <td>{{ $company->created_at }}</td>
+                <td>{{ $company->updated_at }}</td>
+                <td>
+                    <a href="{{ url("/company/delete/$company->id") }}" class="btn btn-danger" title="{{ __('Delete') }}"><i class="las la-trash-alt"></i></a>
+                </td>
+            </tr>
+            @endforeach
+            </tbody>
+            </table>
+        </div>
+    </div><!--./card-body-->
+</div><!--./card-->
 @endsection
