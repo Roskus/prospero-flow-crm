00ce2959a7b8400acaf30f072d4e4d6a96f2418d
Antonio <elguitarraverde@hotmail.com>
2023-02-13 10:18:34 +0100

    test(ticket): add TicketSaveControllerTest (#90)

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use App\Models\Company;
@@ -27,7 +29,7 @@ class TicketFactory extends Factory
             'created_by' => User::factory(),
             'assigned_to' => User::factory(),
             'priority' => $this->faker->randomElement(['low', 'normal', 'high', 'urgent']),
-            'type' => $this->faker->randomElement(['', 'question', 'incident', 'issue']),
+            'type' => $this->faker->randomElement(['question', 'incident', 'issue']),
             'status' => $this->faker->randomElement(['new', 'assigned', 'duplicated', 'closed']),
             'closed_at' => $this->faker->randomElement([null, $this->faker->dateTime()]),
             'deleted_at' => $this->faker->randomElement([null, $this->faker->dateTime()]),
