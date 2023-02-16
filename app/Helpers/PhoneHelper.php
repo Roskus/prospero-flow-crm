97bbbc8ed430a4e74ea3058646ff42a2a5bfa973
Gustavo Novaro <gnovaro@gmail.com>
2023-02-16 12:57:20 +0100

    refactor(helper): Code style replacing
    replacing switch with new macth case

--- app/Helpers/PhoneHelper.php
+++ app/Helpers/PhoneHelper.php
@@ -10,10 +10,11 @@ class PhoneHelper
     {
         $length = strlen($phone);
         $country_code = substr($phone, 0, 3);
+
         return match ($length) {
-            11 => $country_code . '-' . substr($phone, 3, 4) . '-' . substr($phone, 7, 4),
-            12 => $country_code . '-' . substr($phone, 3, 3) . '-' . substr($phone, 6, 3)
-                . '-' . substr($phone, 9, 3),
+            11 => $country_code.'-'.substr($phone, 3, 4).'-'.substr($phone, 7, 4),
+            12 => $country_code.'-'.substr($phone, 3, 3).'-'.substr($phone, 6, 3)
+                .'-'.substr($phone, 9, 3),
             default => $phone,
         };
     }
