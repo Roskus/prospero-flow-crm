52244395a7e216f3fe508d7420c0f0fb560f6839
Gustavo Novaro <gnovaro@gmail.com>
2023-02-21 12:33:16 +0100

    refactor(order): Refactor order to use repository

--- app/Http/Controllers/Order/OrderSaveController.php
+++ app/Http/Controllers/Order/OrderSaveController.php
@@ -4,40 +4,22 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Order;
 
-use App\Http\Controllers\MainController;
-use App\Models\Order;
+use App\Http\Controllers\Controller;
+use App\Repositories\OrderRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
-class OrderSaveController extends MainController
+class OrderSaveController extends Controller
 {
+    private OrderRepository $orderRepository;
+
+    public function __construct(OrderRepository $orderRepository)
+    {
+        $this->orderRepository = $orderRepository;
+    }
+
     public function save(Request $request)
     {
-        $order = new Order();
-        $order->setCompanyId((int) Auth::user()->company_id);
-        $order->setCustomerId((int) $request->customer_id);
-        $order->setAmount((float) $request->total);
-        try {
-            $status = $order->save();
-        } catch (\Throwable $t) {
-            Log::error($t->getMessage());
-        }
-
-        if ($request->items) {
-            foreach ($request->items as $requestItem) {
-                try {
-                    $item = new Order\Item();
-                    $item->order_id = $order->id;
-                    $item->product_id = $requestItem['product_id'];
-                    $item->quantity = $requestItem['quantity'];
-                    $item->unit_price = $requestItem['price'];
-                    $order->items()->save($item);
-                } catch (\Throwable $t) {
-                    Log::error($t->getMessage());
-                }
-            }
-        }
+        $order = $this->orderRepository->save($request->all());
 
         return redirect('order');
     }
---
+++ app/Repositories/OrderRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class OrderRepository
+{
+    public function save(array $data): ?Order
+    {
+        if (empty($data['id'])) {
+            $order = new Order();
+            $order->created_at = now();
+        } else {
+            $order = Order::find($data['id']);
+        }
+
+        $order->setCompanyId((int) Auth::user()->company_id);
+        $order->setCustomerId((int) $data['customer_id']);
+        $order->setAmount((float) $data['total']);
+        try {
+            $status = $order->save();
+        } catch (\Throwable $t) {
+            Log::error($t->getMessage());
+        }
+
+        if ($data['items']) {
+            foreach ($data['items'] as $requestItem) {
+                try {
+                    $item = new Order\Item();
+                    $item->order_id = $order->id;
+                    $item->product_id = $requestItem['product_id'];
+                    $item->quantity = $requestItem['quantity'];
+                    $item->unit_price = $requestItem['price'];
+                    $order->items()->save($item);
+                } catch (\Throwable $t) {
+                    Log::error($t->getMessage());
+                }
+            }
+        }
+
+        return $order;
+    }
+}
