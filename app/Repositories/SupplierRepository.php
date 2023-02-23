19f35c2bf5315d426b64c5bde8965695dd8c1f4e
elguitarraverde <elguitarraverde@users.noreply.github.com>
2023-02-23 20:31:56 +0000

    PHP Linting (Pint)

--- app/Repositories/SupplierRepository.php
+++ app/Repositories/SupplierRepository.php
@@ -14,14 +14,15 @@ class SupplierRepository
         $supplier = (empty($data['id'])) ? new Supplier() : Supplier::find($data['id']);
         $supplier->company_id = Auth::user()->company_id;
         $supplier->name = $data['name'];
-        $supplier->business_name = (!empty($data['business_name'])) ? $data['business_name'] : null;
-        $supplier->phone = (!empty($data['phone'])) ? $data['phone'] : null ;
-        $supplier->vat = (!empty($data['vat'])) ? $data['vat'] : null;
-        $supplier->email = (!empty($data['email'])) ? $data['email'] : null;
-        $supplier->website = (!empty($data['website'])) ? $data['website'] : null;
+        $supplier->business_name = (! empty($data['business_name'])) ? $data['business_name'] : null;
+        $supplier->phone = (! empty($data['phone'])) ? $data['phone'] : null;
+        $supplier->vat = (! empty($data['vat'])) ? $data['vat'] : null;
+        $supplier->email = (! empty($data['email'])) ? $data['email'] : null;
+        $supplier->website = (! empty($data['website'])) ? $data['website'] : null;
         $supplier->country_id = $data['country_id'];
         $supplier->updated_at = now();
         $supplier->save();
+
         return $supplier;
     }
 }
