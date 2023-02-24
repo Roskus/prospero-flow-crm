bcfd0111c14130cefcc589a5bd961ac9ad85af56
Gustavo Novaro <gnovaro@users.noreply.github.com>
2023-02-24 19:31:23 +0100

    Fix #105

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        Blade::withoutDoubleEncoding();
+        //Custom        
         Paginator::useBootstrapFive();
     }
 }
