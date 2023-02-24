98897ef66371a5ca7acdbae7d6a02c91085b3c8f
elguitarraverde <elguitarraverde@users.noreply.github.com>
2023-02-24 19:40:01 +0000

    PHP Linting (Pint)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //Custom        
+        //Custom
         Paginator::useBootstrapFive();
     }
 }
