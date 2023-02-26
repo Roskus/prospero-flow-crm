160e09517621309c420df248cff2652b993cf252
Gustavo Novaro <gnovaro@gmail.com>
2023-02-26 03:02:39 +0100

    feat(lead/customer): Add Lat Log for Lead and Customer geolocation

---
+++ database/migrations/2023_02_25_231425_alter_customer_table_add_latitude_logitude.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            $table->decimal('latitude', 10, 8)->nullable()->after('industry_id');
+            $table->decimal('longitude', 10, 8)->nullable()->after('latitude');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            $table->dropColumn('latitude');
+            $table->dropColumn('longitude');
+        });
+    }
+};
