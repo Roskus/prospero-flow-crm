4765445d2576623702e531fb9202f39721731748
Antonio <elguitarraverde@hotmail.com>
2023-03-01 21:42:10 +0100

    test(lead): add LeadSaveControllerTest (#110)
    * test(lead): add LeadSaveControllerTest
    
    * PHP Linting (Pint)
    
    ---------
    
    Co-authored-by: elguitarraverde <elguitarraverde@users.noreply.github.com>
    Co-authored-by: Gustavo Novaro <gnovaro@users.noreply.github.com>

---
+++ tests/Feature/Controllers/Lead/LeadSaveControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Controllers\Lead;
+
+use App\Models\Lead;
+use App\Models\User;
+use Tests\TestCase;
+
+class LeadSaveControllerTest extends TestCase
+{
+    /** @test */
+    public function it_can_save_lead(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $data = Lead::factory()->create()->toArray();
+        $data['tags'] = 'one, two';
+
+        $response = $this->post('lead/save', $data);
+
+        $response->assertRedirect('/lead');
+        $this->equalTo(Lead::all()->last(), $data);
+    }
+
+    /** @test */
+    public function it_can_update_lead(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $data = Lead::factory()->create()->toArray();
+        $data['tags'] = 'one, two';
+        unset($data['id']);
+
+        $response = $this->post('lead/save', $data);
+
+        $response->assertRedirect('/lead');
+        $this->equalTo(Lead::all()->last(), $data);
+    }
+}
