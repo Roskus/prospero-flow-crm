e03c972ce645536a8c6b164460c9e5be58b5c861
Gustavo Novaro <gnovaro@gmail.com>
2023-03-01 22:06:19 +0100

    fix(contact): Fix contact model declaration
    Fix save vCard

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -35,25 +35,25 @@ class Contact extends Model
     ];
 
     #[OAT\Property(description:'First name of the contact', type: 'string', example: 'John')]
-    protected ?string $first_name;
+    protected ?string $first_name = null;
 
     #[OAT\Property(description:'Lastname of the contact', type: 'string', example: 'Smith')]
-    protected ?string $last_name;
+    protected ?string $last_name = null;
 
     #[OAT\Property(description:'Phone of the contact', type: 'string', example: '3400000000')]
-    protected ?string $phone;
+    protected ?string $phone = null;
 
     #[OAT\Property(description:'Email of the contact', type: 'string', format: 'email', example: 'john.smith@gmail.com')]
-    protected ?string $email;
+    protected ?string $email = null;
 
     #[OAT\Property(description:'LinkedIn of the contact', type: 'string', example: 'https://linkedin.com/john.smith')]
-    protected ?string $linkedin;
+    protected ?string $linkedin = null;
 
     #[OAT\Property(description:'Country of the contact', type: 'string', example: 'uk')]
-    protected ?string $country;
+    protected ?string $country = null;
 
     #[OAT\Property(description:'Notes of the contact', type: 'string', example: 'Test comment')]
-    protected ?string $notes;
+    protected ?string $notes = null;
 
     public function lead()
     {
