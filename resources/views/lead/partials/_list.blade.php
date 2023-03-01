33c88d3d17fc7256f786e30163abce70e45447cf
Antonio <elguitarraverde@hotmail.com>
2023-03-01 17:45:41 +0100

    feat(customer): add Customer list box on dashboard (#122)

---
+++ resources/views/lead/partials/_list.blade.php
@@ -0,0 +1,63 @@
+<div class="card mb-3">
+    <div class="card-header">
+        <h4 class="d-flex d-flex justify-content-between m-0">
+            <a href="{{ $url }}" class="text-decoration-none text-body">{{ $title }}</a>
+            <span class="badge rounded-pill text-bg-success">{{ $leads->count() }}</span>
+        </h4>
+    </div>
+    <div class="card-body table-responsive">
+        @if(!empty($leads))
+        <table class="table table-bordered table-striped table-hover">
+            <tbody>
+                <thead>
+                    <tr>
+                        <th>{{ __('Name') }}</th>
+                        <th>{{ __('Business name') }}</th>
+                        <th>{{ __('Phone') }}</th>
+                        <th>{{ __('Mobile') }}</th>
+                        <th>E-mail</th>
+                        <th>{{ __('Website') }}</th>
+                        <th>{{ __('Status') }}</th>
+                        <th>{{ __('Created at') }}</th>
+                    </tr>
+                </thead>
+            <tbody>
+                @foreach($leads as $lead)
+                <tr>
+                    <td>
+                        <a href="{{ $url }}/update/{{ $lead->id }}">{{ $lead->name }}</a>
+                    </td>
+                    <td>{{ $lead->business_name }}</td>
+                    <td>
+                        @if($lead->phone)
+                        <a href="tel:{{ $lead->phone }}" target="_blank">{{ $lead->phone }}</a>
+                        @endif
+                    </td>
+                    <td>
+                        @if($lead->mobile)
+                        <a href="https://api.whatsapp.com/send/?phone={{ $lead->mobile }}&text={{ __('Hello') }}" target="_blank">{{ $lead->mobile }}</a>
+                        @endif
+                    </td>
+                    <td>
+                        @if($lead->email)
+                        <a href="mailto:{{ $lead->email }}">{{ $lead->email }}</a>
+                        @endif
+                    </td>
+                    <td>
+                        <a href="{{ $lead->website }}" target="_blank">{{ $lead->website }}</a>
+                    </td>
+                    <td class="text-center">
+                        <span class="badge {{ App\Helpers\LeadStatus::renderBadge($lead->status) }}">
+                            {{ __(ucfirst($lead->status)) }}
+                        </span>
+                    </td>
+                    <td>{{ $lead->created_at->format('d/m/Y H:i') }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @endif
+    </div>
+    <!--./card-body-->
+</div>
+<!--./card-->
