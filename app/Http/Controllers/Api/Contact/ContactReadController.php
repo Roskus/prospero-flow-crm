e652d2c380de2cb33abd47716a80e006a0412e8c
Gustavo Novaro <gnovaro@users.noreply.github.com>
2023-03-04 22:57:12 +0100

    Improve contact read controller

--- app/Http/Controllers/Api/Contact/ContactReadController.php
+++ app/Http/Controllers/Api/Contact/ContactReadController.php
@@ -1,14 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api\Contact;
 
 use App\Models\Contact;
+use OpenApi\Annotations as OA;
 
 class ContactReadController
 {
+    /**
+     * @OA\Get(
+     *     path="/contact/{id}",
+     *     summary="Get a contact",
+     *     tags={"Contact"},
+     *     security={{"bearerAuth": {} }},
+     *     @OA\Response(response="404", description="Contact not found"),
+     *     @OA\Response(response="200", description="Contact retrived successfully")
+     * )
+     */
     public function read($id)
     {
-        // TODO TRY CATCH
-        return response()->json(Contact::find($id));
+        $contact = Contact::find($id);
+        return response()->json(['contact' => $contact], ($contact) ? 200 : 404;
     }
 }
