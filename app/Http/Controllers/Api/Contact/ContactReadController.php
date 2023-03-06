d0777bd54b0e65c83ecfbe21500e6ebac7767956
Gustavo Novaro <gnovaro@users.noreply.github.com>
2023-03-06 13:45:59 +0100

    Update api doc missing id param
    Improve code style

--- app/Http/Controllers/Api/Contact/ContactReadController.php
+++ app/Http/Controllers/Api/Contact/ContactReadController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api\Contact;
 
 use App\Models\Contact;
 use OpenApi\Annotations as OA;
+use Illuminate\Http\JsonResponse;
 
 class ContactReadController
 {
@@ -15,14 +16,22 @@ class ContactReadController
      *     summary="Get a contact",
      *     tags={"Contact"},
      *     security={{"bearerAuth": {} }},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Id of Contact",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
      *     @OA\Response(response="404", description="Contact not found"),
      *     @OA\Response(response="200", description="Contact retrived successfully")
      * )
+     *
+     * @return JsonResponse     
      */
-    public function read($id)
+    public function read(int $id)
     {
         $contact = Contact::find($id);
-
         return response()->json(['contact' => $contact], ($contact) ? 200 : 404);
     }
 }
