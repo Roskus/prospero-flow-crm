10f024c4051fe85b20bcdb524c2716e2f6ca0881
Antonio <elguitarraverde@hotmail.com>
2023-03-09 11:32:57 +0100

    test: fix tests email and product

--- tests/Feature/Controllers/Product/ProductSaveControllerTest.php
+++ tests/Feature/Controllers/Product/ProductSaveControllerTest.php
@@ -1,8 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature\Controllers\Product;
 
+use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class ProductSaveControllerTest extends TestCase
@@ -10,16 +15,19 @@ class ProductSaveControllerTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_can_save_a_product()
+    public function it_can_save_a_product(): void
     {
         $user = $this->signIn();
         $this->actingAs($user);
 
-        $name = fake()->name();
-        $response = $this->post('product/save', ['name' => $name, 'cost' => 1, 'price' => 1]);
+        $product = Product::factory()->make()->toArray();
+        $product['photo'] = UploadedFile::fake()->image('photo.jpg');
+        $product['expiration_date'] = Carbon::tomorrow()->format('Y-m-d');
+
+        $response = $this->post('product/save', $product);
         $response->assertRedirect('/product');
 
         $response = $this->get('/product');
-        $response->assertSee($name);
+        $response->assertSee($product['name']);
     }
 }
