d0bdc5239b6b951d870352f8d668afa6ab53c6e0
Antonio <elguitarraverde@hotmail.com>
2023-03-09 11:31:10 +0100

    test(factory): add factories

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'company_id' => Company::factory(),
+            'name' => $this->faker->word(),
+        ];
+    }
+}
