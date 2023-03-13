86927844ce3745a84862b52d1723dd2348edd7ef
Gustavo Novaro <gnovaro@gmail.com>
2023-03-13 21:06:57 +0100

    fix(global): Fix force https for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,27 +3,28 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register() : void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot() : void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         //Custom
         Paginator::useBootstrapFive();
     }
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
 
-const APP_VERSION = '2.1.5';
+const APP_VERSION = '2.1.6';
