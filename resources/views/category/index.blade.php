a4a723860c88a2617c5aa0d4875f1bc2cf5c36ff
Gustavo Novaro <gnovaro@gmail.com>
2023-03-15 17:22:35 +0100

    refactor(global): Improve UI
    - Add Card to categories page
    - Split company page in diffrent card by section

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -9,31 +9,33 @@
         <a href=" {{ url('/category/create') }}" class="btn btn-primary">{{ __('New') }}</a>
     </div>
 
-    <div class="mt-2">
-        <table class="table table-bordered table-striped table-bordered table-hover">
-        <thead>
+    <div class="card mt-2">
+        <div class="card-body">
+            <table class="table table-bordered table-striped table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>{{ __('Name') }}</th>
+                    <th>{{ __('Actions')}}</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($categories as $category)
             <tr>
-                <th>{{ __('Name') }}</th>
-                <th>{{ __('Actions')}}</th>
+                <td>
+                    <a href="{{ url('/category/update/'.$category->id) }}">{{ $category->name }}</a>
+                </td>
+                <td>
+                    <a href="{{ url('/category/update/'.$category->id) }}" class="btn btn-xs btn-warning text-white">
+                        <i class="las la-pen"></i>
+                    </a>
+                    <a onclick="Category.delete({{ $category->id }}, '{{ $category->name }}');" class="btn btn-xs btn-danger">
+                        <i class="las la-trash-alt"></i>
+                    </a>
+                </td>
             </tr>
-        </thead>
-        <tbody>
-        @foreach ($categories as $category)
-        <tr>
-            <td>
-                <a href="{{ url('/category/update/'.$category->id) }}">{{ $category->name }}</a>
-            </td>
-            <td>
-                <a href="{{ url('/category/update/'.$category->id) }}" class="btn btn-xs btn-warning text-white">
-                    <i class="las la-pen"></i>
-                </a>
-                <a onclick="Category.delete({{ $category->id }}, '{{ $category->name }}');" class="btn btn-xs btn-danger">
-                    <i class="las la-trash-alt"></i>
-                </a>
-            </td>
-        </tr>
-        @endforeach
-        </tbody>
-        </table>
+            @endforeach
+            </tbody>
+            </table>
+        </div>
     </div>
 @endsection
