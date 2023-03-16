673ced4aa01e5eba0995559881b90dfe5288cef6
Antonio <elguitarraverde@hotmail.com>
2023-03-16 10:20:23 +0100

    refactor(style): improve sidebar menu (#152)

--- resources/views/menu/sidebar.blade.php
+++ resources/views/menu/sidebar.blade.php
@@ -1,67 +1,44 @@
-<div class="d-flex flex-column flex-shrink-0 bg-light d-none d-sm-block d-print-none" style="width: 4.5rem;">
-    <a href="/" class="d-block p-3 link-dark text-decoration-none" title="Icon-only" data-bs-toggle="tooltip" data-bs-placement="right">
-        <svg class="bi" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-        <span class="visually-hidden">Icon-only</span>
-    </a>
+@php $menu = include(resource_path('views'.DIRECTORY_SEPARATOR.'menu'.DIRECTORY_SEPARATOR.'menu.php')); @endphp
 
-    <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-        <li class="nav-item">
-            <a href="{{ url('/') }}" class="nav-link py-2 border-bottom @if(Request::path() == '/') active @endif" aria-current="page" title="Dashoard" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-compass fs-1"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('/lead') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(),'lead')) active @endif" title="{{ __('Leads') }}" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-filter fs-1"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('/customer') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'customer')) active @endif" title="{{ __('Customers') }}" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-user-alt fs-1"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('/order') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'order')) active @endif" title="{{ __('Orders') }}" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-receipt fs-1"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('/product') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'product')) active @endif" title="{{ __('Products') }}" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-box fs-1"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('/calendar') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'calendar')) active @endif" title="{{ __('Calendar') }}" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-calendar fs-1"></i>
-            </a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle px-0 @if(in_array(Request::path(), ['email', 'campaign'])) active @endif" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><i class="las la-envelope-open-text fs-1"></i></a>
-            <ul class="dropdown-menu">
-                <li><a href="{{ url('/email') }}" class="dropdown-item @if(Request::path() == 'email') active @endif fs-6"><i class="las la-envelope-open-text"></i> E-mail</a></li>
-                <li><a href="{{ url('/campaign') }}" class="dropdown-item @if(Request::path() == 'campaign') active @endif fs-6"><i class="las la-mail-bulk"></i> {{ __('Campaign') }}</a></li>
-            </ul>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('/accounting') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'accounting')) active @endif" title="{{ __('Accounting') }}" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-coins fs-1"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('/supplier') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(),'supplier')) active @endif" title="{{ __('Suppliers') }}" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-dolly fs-1"></i>
-            </a>
-        </li>
-        <li class="nav-item fs-5">
-            <a href="{{ url('/ticket') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'ticket')) active @endif" title="{{ __('Tickets') }}" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-tools fs-1"></i>
-            </a>
-        </li>
-        <li class="nav-item fs-5">
-            <a href="{{ url('/report') }}" class="nav-link py-2 border-bottom @if(str_contains(Request::path(), 'report')) active @endif" title="{{ __('Reports') }}" data-bs-toggle="tooltip" data-bs-placement="right">
-                <i class="las la-chart-pie fs-1"></i>
-            </a>
-        </li>
+<div class="d-flex flex-column flex-shrink-0 bg-light d-none d-sm-block d-print-none sidebar rounded">
+    <ul class="nav nav-pills nav-flush flex-column mb-auto ">
+        
+        @foreach ($menu as $item)
+            @if(isset($item['is_drop_down']))
+                {{-- DROPDOWN --}}
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div class="d-flex align-items-center">
+                            <i class="{{ $item['icon_class'] }} fs-2"></i>
+                            <span class="ps-2">{{ $item['title'] }}</span>
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu">
+                        @foreach($item['children'] as $child)
+                            <li>
+                                <a class="dropdown-item @if(Request::url() == $child['url']) active @endif fs-6" href="{{ $child['url'] }}">
+                                    <div class="d-flex align-items-center">
+                                        <i class="{{ $child['icon_class'] }}"></i>
+                                        <span class="ps-2">{{ $child['title'] }}</span>
+                                    </div>
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            @else
+                {{-- LINK --}}
+                <li class="nav-item">
+                    <a href="{{ $item['url'] }}" class="nav-link px-3 py-1  @if(Request::url() == $item['url']) active @endif" title="{{ $item['title'] }}" data-bs-toggle="tooltip" data-bs-placement="right">
+                        <div class="d-flex align-items-center">
+                            <i class="{{ $item['icon_class'] }} fs-2"></i>
+                            <span class="ps-2">{{ $item['title'] }}</span>
+                        </div>
+                    </a>
+                </li>
+            @endif        
+        @endforeach
+
     </ul>
     <div class="dropdown border-top">
         <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
@@ -74,7 +51,9 @@
         <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
             <li><a class="dropdown-item" href="{{ url('/profile') }}"><i class="las la-user-tie"></i> {{ __('Profile') }}</a></li>
             <li><a class="dropdown-item" href="{{ url('/setting') }}"><i class="las la-cogs"></i> {{ __('Settings') }}</a></li>
-            <li><hr class="dropdown-divider"></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
             <li>
                 <a class="dropdown-item" onclick="Hammer.exit()" href="#"><i class="las la-door-open"></i> {{ __('Exit') }}</a>
             </li>
