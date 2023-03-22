0b8cfd0b00ae8e2f71d0c0091b8a3ab7b0fc6c6b
Gustavo Novaro <gnovaro@gmail.com>
2023-03-22 20:44:05 +0100

    fix(CI/CD): Fix deploy script send last commit to sentry

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,10 @@ sudo chmod 775 storage/ -R
 GIT_HASH=git rev-parse HEAD
 # Get APP_VERSION
 VERSION=$(sed -n "s/.*APP_VERSION = '\(.*\)';/\1/p" version.php)
+export SENTRY_ORG=roskus-fo
+#debug|info
+export SENTRY_LOG_LEVEL=debug
 # Notify sentry new release version
-sentry-cli releases -o roskus-fo new -p prospectflow $VERSION
+sentry-cli releases new -p prospectflow $VERSION
 # Associate commits with the release
 sentry-cli releases set-commits --auto $VERSION
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
 
-const APP_VERSION = '2.2.2';
+const APP_VERSION = '2.2.3';
