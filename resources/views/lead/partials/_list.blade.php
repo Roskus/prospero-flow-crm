4c687f74a67b98f832cdcc9af659fd4c7da8b857
Gustavo Novaro <gnovaro@gmail.com>
2023-03-29 00:42:53 +0200

    refactor(global): Small improvements
    - Fix Dashboard table. Improve UI colors, fonts, grid
    - WIP: Improve CampaignSender code style
    - composer add pdf library
    - Add pixel.gif in public folder

--- resources/views/lead/partials/_list.blade.php
+++ resources/views/lead/partials/_list.blade.php
@@ -8,65 +8,66 @@
     <div class="card-body">
         <div class="table-responsive">
         @if(!empty($leads))
-        <table class="table table-bordered table-striped table-hover">
+            <table class="table table-bordered table-striped table-hover">
+            <thead>
+            <tr>
+                <th>{{ __('Name') }}</th>
+                <th>{{ __('Business name') }}</th>
+                <th>{{ __('Phone') }}</th>
+                <th>{{ __('Mobile') }}</th>
+                <th>Email</th>
+                <th>{{ __('Website') }}</th>
+                <th>{{ __('Status') }}</th>
+                <th>{{ __('Created at') }}</th>
+            </tr>
+            </thead>
             <tbody>
-                <thead>
-                    <tr>
-                        <th>{{ __('Name') }}</th>
-                        <th>{{ __('Business name') }}</th>
-                        <th>{{ __('Phone') }}</th>
-                        <th>{{ __('Mobile') }}</th>
-                        <th>E-mail</th>
-                        <th>{{ __('Website') }}</th>
-                        <th>{{ __('Status') }}</th>
-                        <th>{{ __('Created at') }}</th>
-                    </tr>
-                </thead>
-            <tbody>
-                @foreach($leads as $lead)
-                <tr>
-                    <td>
-                        <a href="{{ $url }}/update/{{ $lead->id }}">{{ $lead->name }}</a>
-                    </td>
-                    <td>{{ $lead->business_name }}</td>
-                    <td class="text-center">
-                        @if($lead->phone)
-                        <a href="tel:{{ $lead->phone }}" title="{{ $lead->phone }}" target="_blank">
-                            <i class="las la-phone fs-4"></i>
-                        </a>
-                        @endif
-                    </td>
-                    <td class="text-center">
-                        @if($lead->mobile)
-                        <a href="https://api.whatsapp.com/send/?phone={{ $lead->mobile }}&text={{ __('Hello') }}"
-                           title="{{ $lead->mobile }}" target="_blank">
-                            <i class="las la-mobile fs-4"></i>
-                        </a>
-                        @endif
-                    </td>
-                    <td class="text-center">
-                        @if($lead->email)
-                        <a href="mailto:{{ $lead->email }}" title="{{ $lead->email }}">
-                            <i class="las la-envelope fs-4"></i>
-                        </a>
-                        @endif
-                    </td>
-                    <td class="text-center">
-                        @isset($lead->website)
-                        <a href="{{ $lead->website }}" title="{{ $lead->website }}" target="_blank">
-                            <i class="las la-globe fs-4"></i>
-                        </a>
-                        @endisset
-                    </td>
-                    <td class="text-center">
-                        <span class="badge {{ App\Helpers\LeadStatus::renderBadge($lead->status) }}">
-                            {{ __(ucfirst($lead->status)) }}
-                        </span>
-                    </td>
-                    <td>{{ $lead->created_at->format('d/m/Y H:i') }}</td>
-                </tr>
-                @endforeach
-            </tbody>
+            @foreach($leads as $lead)
+            <tr>
+                <td>
+                    <a href="{{ $url }}/update/{{ $lead->id }}">{{ $lead->name }}</a>
+                </td>
+                <td>{{ $lead->business_name }}</td>
+                <td class="text-center">
+                    @if($lead->phone)
+                    <a href="tel:{{ $lead->phone }}" title="{{ $lead->phone }}" target="_blank" class="link-secondary">
+                        <i class="las la-phone fs-4"></i>
+                    </a>
+                @endif
+                </td>
+                <td class="text-center">
+                    @if($lead->mobile)
+                    <a href="https://api.whatsapp.com/send/?phone={{ $lead->mobile }}&text={{ __('Hello') }}"
+                       title="{{ $lead->mobile }}" target="_blank" class="link-secondary">
+                        <i class="las la-mobile fs-4"></i>
+                    </a>
+                    @endif
+                </td>
+                <td class="text-center">
+                    @if($lead->email)
+                    <a href="mailto:{{ $lead->email }}" title="{{ $lead->email }}" class="link-secondary">
+                        <i class="las la-envelope fs-4"></i>
+                    </a>
+                    @endif
+                </td>
+                <td class="text-center">
+                    @isset($lead->website)
+                    <a href="{{ $lead->website }}" title="{{ $lead->website }}" target="_blank" class="link-secondary">
+                        <i class="las la-globe fs-4"></i>
+                    </a>
+                    @endisset
+                </td>
+                <td class="text-center">
+                    <span class="badge {{ App\Helpers\LeadStatus::renderBadge($lead->status) }}">
+                        {{ __(ucfirst($lead->status)) }}
+                    </span>
+                </td>
+                <td>
+                    <small class="text-muted">{{ $lead->created_at->format('d/m/Y H:i') }}</small>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
         </table>
         @endif
         </div>
