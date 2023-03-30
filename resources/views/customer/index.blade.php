8f82a44d5f4d16fbf731413bca7a8b6dc881854b
Antonio <elguitarraverde@hotmail.com>
2023-03-30 22:47:05 +0200

    feat(filters): add phone filter to lead and customer index (#181)

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -40,37 +40,7 @@
                     </button>
                 </div>
             </div>
-            <div class="card mt-2">
-                <div class="card-header">{{ __('Advanced search') }}</div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-6 col-md-3">
-                            @include('components.seller', ['seller_id' => !empty($seller_id) ? $seller_id : null])
-                        </div>
-                        <div class="col-sm-6 col-md-3">
-                            <label for="status">{{ __('Status') }}</label>
-                            <select name="status" id="status"  class="form-select">
-                                <option value=""></option>
-                                @foreach($statuses as $key => $status)
-                                <option value="{{ $key }}">{{ __($status) }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-sm-6 col-md-3">
-                            @include('components.country', ['country_id' => !empty($country_id) ? $country_id : null])
-                        </div>
-                        <div class="col-sm-6 col-md-3">
-                            <label for="industry_id">{{ __('Industry') }}</label>
-                            <select name="industry_id" id="industry_id" class="form-select">
-                                <option value=""></option>
-                                @foreach($industries as $industry)
-                                    <option value="{{ $industry->id }}">{{ $industry->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div><!--./row-->
-                </div>
-            </div>
+            @include('lead_customer.partials.filters')
         </form>
     </div>
 </div>
