b65872689fbc1de27dec7c1a8fd23fd19a142b76
Antonio <elguitarraverde@hotmail.com>
2023-04-12 22:13:45 +0200

    test(lead): add lead api tests

---
+++ tests/Feature/Controllers/Api/Lead/LeadReadControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Controllers\Api\Lead;
+
+use App\Models\Lead;
+use App\Models\User;
+use Tests\TestCase;
+
+class LeadReadControllerTest extends TestCase
+{
+    /** @test */
+    public function it_can_read_lead(): void
+    {
+        $this->actingAs(User::factory()->create(), 'api');
+        $lead = Lead::factory()->create(['company_id' => auth()->user()->company_id, 'seller_id' => auth()->id()]);
+
+        $response = $this->get('/api/lead/'.$lead->id);
+
+        $this->assertEquals(array_except($response->json()['lead'], ['seller', 'industry', 'country']), $lead->toArray());
+    }
+
+    /** @test */
+    public function it_not_found_a_lead(): void
+    {
+        $this->actingAs(User::factory()->create(), 'api');
+        
+        $response = $this->get('/api/lead/9999');
+
+        $response->assertNotFound();
+        $response->assertExactJson(['lead' => null]);
+    }
+}
