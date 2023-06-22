2cec495041716ea3f4bf8a92effbdc421212fd1a
Gustavo Novaro <gnovaro@gmail.com>
2023-06-22 16:29:52 +0200

    fix(email): Fix email from name

---
+++ app/Http/Controllers/Api/Email/EmailCreateController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api\Email;
+
+use App\Repositories\EmailRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class EmailCreateController
+{
+    public function __construct(EmailRepository $emailRepository)
+    {
+    }
+
+    public function create(Request $request): JsonResponse
+    {
+        $status = 400;
+
+        return response()->json(['company' => $email, $status]);
+    }
+}
