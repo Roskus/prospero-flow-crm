2783a6a821c4697fd2784be4e2eb400949a7dba9
Gustavo Novaro <gnovaro@gmail.com>
2023-06-29 23:44:03 +0200

    refactor(contact): Improve contact UI

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -32,20 +32,20 @@
         <td class="text-center">
             @if($contact->mobile)
                 <a href="https://api.whatsapp.com/send/?phone={{ $contact->mobile }}&text={{ __('Hello') }}"
-                   title="{{ $contact->mobile }}">
+                   title="{{ $contact->mobile }}" class="link-secondary">
                     <i class="las la-mobile fs-4"></i>
                 </a>
             @endif
         </td>
         <td class="text-center">
             @if($contact->email)
-                <a href="mailto:{{ $contact->email }}" title="{{ $contact->email }}">
-                    <i class="las la-envelope fs-3"></i>
+                <a href="mailto:{{ $contact->email }}" title="{{ $contact->email }}" class="link-secondary">
+                    <i class="las la-envelope fs-4"></i>
                 </a>
                 @if($contact->email_verified == 1)
-                    <i class="las la-check-circle text-success"></i>
+                <i class="las la-check-circle text-success"></i>
                 @elseif($contact->email_verified == 3)
-                    <i class="las la-times-circle text-danger"></i>
+                <i class="las la-times-circle text-danger"></i>
                 @endif
             @endif
         </td>
