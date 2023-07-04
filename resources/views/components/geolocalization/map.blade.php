dbc79af3e710b96af199c0b83c68a0683d1ad692
Gustavo Novaro <gnovaro@gmail.com>
2023-07-05 01:17:06 +0200

    refactor(map): Update leaflet minor version

--- resources/views/components/geolocalization/map.blade.php
+++ resources/views/components/geolocalization/map.blade.php
@@ -1,7 +1,13 @@
 <div id="map" class="h-100 rounded border" style="min-height: 340px"></div>
 
 @push('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/leaflet.min.js" integrity="sha512-Io0KK/1GsMMQ8Vpa7kIJjgvOcDSwIqYuigJEYxrrObhsV4j+VTOQvxImACNJT5r9O4n+u9/58h7WjSnT5eC4hA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+      crossorigin="">
+
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+        crossorigin=""></script>
 <script>
     "use strict";
 
@@ -79,8 +85,4 @@
         var marker = L.marker([latitude, longitude]).addTo(map);
     }
 </script>
-@endpush
-
-@push('styles')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/leaflet.min.css" integrity="sha512-KJRB1wUfcipHY35z9dEE+Jqd+pGCuQ2JMZmQPAjwPjXuzz9oL1pZm2cd79vyUgHQxvb9sFQ6f05DIz0IqcG1Jw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endpush
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
 
-const APP_VERSION = '3.4.1';
+const APP_VERSION = '3.4.2';
