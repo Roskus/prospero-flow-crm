0e27aa99e6ffeb2867dad9c76f1781d2b4669790
Gustavo Novaro <gnovaro@gmail.com>
2023-08-02 01:46:06 +0200

    feat(global): Expand session lifetime to 8hs

--- config/session.php
+++ config/session.php
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 480), // 8 hours
 
     'expire_on_close' => false,
 
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
 
-const APP_VERSION = '3.5.8';
+const APP_VERSION = '3.5.9';
