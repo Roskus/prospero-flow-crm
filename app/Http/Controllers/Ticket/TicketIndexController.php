884f929377080d6f6b9d59084f0d26f19abfaeda
Gustavo Novaro <gnovaro@gmail.com>
2024-03-09 16:54:37 +0100

    fix(ticket): Fix load tickets

--- app/Http/Controllers/Ticket/TicketIndexController.php
+++ app/Http/Controllers/Ticket/TicketIndexController.php
@@ -19,7 +19,7 @@ class TicketIndexController extends MainController
 
         $data['tickets'] = $ticket->getAllByCompanyId((int) Auth::user()->company_id, $search);
         $data['search'] = $search;
-        $data['customers'] = Customer::whereCompany((int) Auth::user()->company);
+        $data['customers'] = Customer::whereCompany(Auth::user()->company);
 
         return view('ticket.index', $data);
     }
