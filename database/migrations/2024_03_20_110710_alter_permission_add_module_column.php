8a61f58a3badab9d6f91a6544f62f80a22c20adc
Gustavo Novaro <gnovaro@gmail.com>
2024-03-20 13:50:01 +0100

    refactor(permission): Assign module to a each permision

---
+++ database/migrations/2024_03_20_110710_alter_permission_add_module_column.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('permission', function (Blueprint $table) {
+            $table->bigInteger('module_id')->after('id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('permission', function (Blueprint $table) {
+            $table->dropColumn('module_id');
+        });
+    }
+};
