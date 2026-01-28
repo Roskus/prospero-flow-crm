761c3d852804c1bad754c654667d25a2e0595028
Gustavo Novaro <gnovaro@gmail.com>
2026-01-28 15:12:39 +0100

    fix(ui) Improve accessibility by adding 'for' attributes to labels in bank account form

--- resources/views/bank_account/bank_account.blade.php
+++ resources/views/bank_account/bank_account.blade.php
@@ -11,7 +11,7 @@
                     @include('components.country', ['country_id' => $bank_account->country_id])
                 </div>
                 <div class="col">
-                    <label>{{ __('Bank') }}</label>
+                    <label for="bank_id" class="">{{ __('Bank') }}</label>
                     <select name="bank_id" id="bank_id"  required class="form-select">
                         <option value="">{{ __('Choose') }}</option>
                         @foreach($banks as $bank)
@@ -22,8 +22,8 @@
             </div>
             <div class="row">
                 <div class="col">
-                    <label>{{ __('IBAN') }}</label>
-                    <input type="text" name="iban" value="{{ $bank_account->iban }}" required maxlength="34" class="form-control">
+                    <label for="iban" class="">{{ __('IBAN') }}</label>
+                    <input type="text" name="iban" id="iban" placeholder="ESXX XXXX XXXX XXXX XXXX XXXX" value="{{ $bank_account->iban }}" required maxlength="34" class="form-control">
                 </div>
             </div>
             <div class="row">
