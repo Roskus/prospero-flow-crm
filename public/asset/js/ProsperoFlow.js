be7dab74aefb6fc1b0900b3e4d11e35809a5d700
Gustavo Novaro <gnovaro@gmail.com>
2026-07-06 11:51:00 +0200

    fix(global) Rename js from Old Project Name to new

--- public/asset/js/ProsperoFlow.js
+++ public/asset/js/ProsperoFlow.js
@@ -1,9 +1,12 @@
+/**
+ * Prospero Flow CRM
+ */
 /* Tooltip global bootstrap: false */
 (function () {
-    'use strict'
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    'use strict';
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-        new bootstrap.Tooltip(tooltipTriggerEl)
+        new bootstrap.Tooltip(tooltipTriggerEl);
     });
 })();
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -159,7 +159,7 @@
 <!--JavaScript-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-<script src="/asset/js/ProspectFlow.js"></script>
+<script src="/asset/js/ProsperoFlow.js"></script>
 <script src="/asset/js/Notification.js"></script>
 @auth
 <script>
